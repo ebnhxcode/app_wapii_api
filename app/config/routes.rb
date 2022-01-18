71eaf17593bcdc6975c08e9f97a2ec46f1c309ec
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2022-01-18 14:08:56 -0300

    Update, add routes for the requirements.

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  get '/users' => 'users#index'
+  get '/posts' => 'posts#index'
+  get '/posts/:id' => 'posts#show'
+  get '/posts/:id/comments' => 'posts#show_comments'
+  get '/comments' => 'comments#index'
 
-
+  get '/trending/:n' => 'posts#trending'
+  get '/influencers/:n' => 'users#influencers'
 
 
 
